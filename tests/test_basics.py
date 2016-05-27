9af026229c0bcd067f86671933041545142dae9c
zjryan <zjryan@gmail.com>
2016-05-27 13:32:25 +0800

    add unittest

---
+++ tests/test_basics.py
@@ -0,0 +1,15 @@
+import unittest
+from flask import current_app
+from app import app
+
+class BasicsTestCase(unittest.TestCase):
+    def setUp(self):
+        self.app = app
+        self.app_context = self.app.app_context()
+        self.app_context.push()
+
+    def tearDown(self):
+        self.app_context.pop()
+
+    def test_app_exists(self):
+        self.assertFalse(current_app is None)
